06fc1e0fa54a3143bbe280292c7e776ee208c7f7
karthik_vc <karthvc@gmail.com>
2023-08-17 17:14:19 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      theme: new ThemeData(),
+      home: new FormPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class FormPage extends StatefulWidget {
+  @override
+  _FormPageState createState() => new _FormPageState();
+}
 
+class _FormPageState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: new Text('Form Page'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: new Container(),
     );
   }
 }
